62d3afc6fa83ea859a041b64ea07ddd92b3e64ca
Amanda Fernandes <dcunhaa@yahoo.com>
2018-11-12 10:30:37 -0500

    update

--- vasyaClerk.js
+++ vasyaClerk.js
@@ -1,13 +1,3 @@
-/*
-The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
-
-Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
-
-Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
-
-Return YES, if Vasya can sell a ticket to each person and give the change with the bills he has at hand at that moment. Otherwise return NO.
-*/
-
 function tickets(peopleInLine){
   let cashInHand = [];
   
@@ -26,6 +16,7 @@ function tickets(peopleInLine){
       let i25 = cashInHand.findIndex(c=>c===25);
       if (i50 >= 0  && i25 >=0) {
         cashInHand.splice(i50,1);
+        i25 = cashInHand.findIndex(c=>c===25);
         cashInHand.splice(i25,1);
         return true;
       } else if (i25 >=0) {
